32bdf86a14feefb46f6ff7dc56c3fa191e7a5b27
toluxfash <146040368+toluxfash@users.noreply.github.com>
2025-02-11 19:45:14 +0100

    Update books.py

--- api/routes/books.py
+++ api/routes/books.py
@@ -11,7 +11,7 @@ db = InMemoryDB()
 db.books = {
     1: Book(
         id=1,
-        title="The Hobbit",
+        title="The Hobbi",
         author="J.R.R. Tolkien",
         publication_year=1937,
         genre=Genre.SCI_FI,
